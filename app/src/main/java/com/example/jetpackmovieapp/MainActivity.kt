f289e06770506e368db77825b9be65a0935a801b
hesamkotlin <hesam.naderi.kotlin@gmail.com>
2023-11-15 16:32:44 +0330

    add: refactor composable functions

--- app/src/main/java/com/example/jetpackmovieapp/MainActivity.kt
+++ app/src/main/java/com/example/jetpackmovieapp/MainActivity.kt
@@ -4,40 +4,49 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.jetpackmovieapp.ui.theme.JetPackMovieAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetPackMovieAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+            myApp {
+                mainContent()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun myApp(content: @Composable () -> Unit) {
+    Scaffold(topBar = {
+        TopAppBar(backgroundColor = Color.Magenta, elevation = 5.dp) {
+            Text("Movies")
+        }
+
+    }) {
+        content()
+    }
+}
+
+@Composable
+fun mainContent() {
+    Surface(color = MaterialTheme.colors.background) {
+        Text(text = "hello")
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    JetPackMovieAppTheme {
-        Greeting("Android")
+    myApp {
+        Text(text = "hello")
     }
 }
