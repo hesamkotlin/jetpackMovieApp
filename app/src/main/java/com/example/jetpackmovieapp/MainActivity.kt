81a7eca0f7278e197b7294b55c94a8fd9e821fab
hesamkotlin <hesam.naderi.kotlin@gmail.com>
2023-11-15 18:35:09 +0330

    add: lazy column

--- app/src/main/java/com/example/jetpackmovieapp/MainActivity.kt
+++ app/src/main/java/com/example/jetpackmovieapp/MainActivity.kt
@@ -3,11 +3,19 @@ package com.example.jetpackmovieapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.jetpackmovieapp.ui.theme.JetPackMovieAppTheme
@@ -36,17 +44,55 @@ fun myApp(content: @Composable () -> Unit) {
 }
 
 @Composable
-fun mainContent() {
-    Surface(color = MaterialTheme.colors.background) {
-        Text(text = "hello")
+fun mainContent(
+    movieList: List<String> = listOf(
+        "Avatar",
+        "300",
+        "Harry Potter",
+        "Life"
+    )
+) {
+    Column(modifier = Modifier.padding(12.dp)) {
+        LazyColumn {
+            items(movieList) {
+                movieRow(movie = it)
+            }
+        }
     }
+}
 
+@Composable
+fun movieRow(movie: String) {
+    Card(
+        modifier = Modifier
+            .padding(4.dp)
+            .fillMaxWidth()
+            .height(130.dp),
+        shape = RoundedCornerShape(corner = CornerSize(12.dp)), elevation = 6.dp
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start
+        ) {
+            Surface(
+                modifier = Modifier
+                    .padding(12.dp)
+                    .size(100.dp),
+                shape = RectangleShape,
+                elevation = 4.dp
+            ) {
+                Icon(imageVector = Icons.Default.AccountBox, contentDescription ="movie image" )
+
+            }
+            Text(movie)
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     myApp {
-        Text(text = "hello")
+        mainContent()
     }
 }
