1a6b551ef6e1a227dbdf420610a984e623bf6989
hesamkotlin <hesam.naderi.kotlin@gmail.com>
2023-11-21 23:45:51 +0330

    add: top bar and config sample app

--- app/src/main/java/com/screens/details/DetailsScreen.kt
+++ app/src/main/java/com/screens/details/DetailsScreen.kt
@@ -1,37 +1,51 @@
 package com.screens.details
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
 @Composable
 fun DetailsScreen(navController: NavController, movieData: String?, ticketPrice: Int?) {
-    Surface(
-        modifier = Modifier
-            .fillMaxWidth()
-            .fillMaxHeight()
-    ) {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-        ) {
-            Text(text = movieData.toString(), style = MaterialTheme.typography.h5)
-            Text(text = ticketPrice.toString())
-            Button(onClick = {navController.popBackStack()}) {
-                Text(text = "Go Back")
+
+    Scaffold(topBar = {
+        TopAppBar(backgroundColor = Color.Magenta, elevation = 5.dp) {
+
+            Row(horizontalArrangement = Arrangement.Start) {
+                Icon(
+                    imageVector = Icons.Default.ArrowBack,
+                    contentDescription = "Arrow Back",
+                    modifier = Modifier.clickable {
+                        navController.popBackStack()
+                    })
+                Spacer(modifier = Modifier.width(100.dp))
+                Text(text = "Movie Details")
             }
         }
-
+    }) {
+        Surface(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight()
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(text = movieData.toString(), style = MaterialTheme.typography.h5)
+                Text(text = ticketPrice.toString())
+                Button(onClick = { navController.popBackStack() }) {
+                    Text(text = "Go Back")
+                }
+            }
+        }
     }
-
 
 }
