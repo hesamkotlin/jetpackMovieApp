0a012579092e764e0b39bb1f9dea8d5535f672b7
hesamkotlin <hesam.naderi.kotlin@gmail.com>
2023-11-26 13:52:10 +0330

    add: finish details screen

--- app/src/main/java/com/screens/details/DetailsScreen.kt
+++ app/src/main/java/com/screens/details/DetailsScreen.kt
@@ -1,7 +1,10 @@
 package com.screens.details
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
@@ -11,9 +14,16 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import coil.compose.rememberImagePainter
+import com.model.getMovies
+import com.widgets.movieRow
 
 @Composable
-fun DetailsScreen(navController: NavController, movieData: String?, ticketPrice: Int?) {
+fun DetailsScreen(navController: NavController, movieId: String?, ticketPrice: Int?) {
+
+    val newMovieList = getMovies().filter { movie ->
+        movie.id == movieId
+    }
 
     Scaffold(topBar = {
         TopAppBar(backgroundColor = Color.Magenta, elevation = 5.dp) {
@@ -37,13 +47,27 @@ fun DetailsScreen(navController: NavController, movieData: String?, ticketPrice:
         ) {
             Column(
                 horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
+                verticalArrangement = Arrangement.Top
             ) {
-                Text(text = movieData.toString(), style = MaterialTheme.typography.h5)
-                Text(text = ticketPrice.toString())
-                Button(onClick = { navController.popBackStack() }) {
-                    Text(text = "Go Back")
+                movieRow(movie = newMovieList[0])
+                Spacer(modifier = Modifier.height(8.dp))
+                Divider()
+                Text(text = "Movie Images")
+                LazyRow {
+                    items(newMovieList[0].images) { images ->
+                        Card(
+                            modifier = Modifier
+                                .padding(12.dp)
+                                .size(240.dp), elevation = 5.dp
+                        ) {
+                            Image(
+                                painter = rememberImagePainter(data = images),
+                                contentDescription = "Images"
+                            )
+                        }
+                    }
                 }
+//                Text(text = ticketPrice.toString())
             }
         }
     }
