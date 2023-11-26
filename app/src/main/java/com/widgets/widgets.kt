e21ef4e29cd174d2697a0f93f72a96255e29ed4f
hesamkotlin <hesam.naderi.kotlin@gmail.com>
2023-11-26 13:35:14 +0330

    add: more info to movie card

--- app/src/main/java/com/widgets/widgets.kt
+++ app/src/main/java/com/widgets/widgets.kt
@@ -1,5 +1,6 @@
 package com.widgets
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
@@ -7,13 +8,20 @@ import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountBox
-import androidx.compose.runtime.Composable
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.rememberImagePainter
 import coil.transform.CircleCropTransformation
 import com.model.Movie
@@ -22,11 +30,16 @@ import com.model.getMovies
 @Preview
 @Composable
 fun movieRow(movie: Movie = getMovies()[0], onItemClick: (String) -> Unit = {}) {
+
+    var expanded by remember {
+        mutableStateOf(false)
+    }
+
     Card(
         modifier = Modifier
             .padding(4.dp)
             .fillMaxWidth()
-            .height(130.dp)
+//            .height(130.dp)
             .clickable {
                 onItemClick(movie.id)
             },
@@ -56,9 +69,53 @@ fun movieRow(movie: Movie = getMovies()[0], onItemClick: (String) -> Unit = {}) 
             Column(modifier = Modifier.padding(4.dp)) {
                 Text(movie.title, style = MaterialTheme.typography.h6)
                 Text("director:${movie.director}", style = MaterialTheme.typography.caption)
-                Text("released: ${movie.year}")
-            }
+                Text("released: ${movie.year}", style = MaterialTheme.typography.caption)
 
+                AnimatedVisibility(visible = expanded) {
+                    Column {
+                        Text(buildAnnotatedString {
+                            withStyle(
+                                style = SpanStyle(
+                                    color = Color.DarkGray,
+                                    fontSize = 13.sp
+                                )
+                            ) {
+                                append("plot: ")
+                            }
+                            withStyle(
+                                style = SpanStyle(
+                                    color = Color.DarkGray, fontSize = 13.sp,
+                                    fontWeight = FontWeight.Light
+                                )
+                            ) {
+                                append(movie.plot)
+                            }
+                        }, modifier = Modifier.padding(6.dp))
+                        Divider(modifier = Modifier.padding(3.dp))
+                        Text(
+                            text = "Director: ${movie.director}",
+                            style = MaterialTheme.typography.caption
+                        )
+                        Text(
+                            text = "Actors: ${movie.actors}",
+                            style = MaterialTheme.typography.caption
+                        )
+                        Text(
+                            text = "Rating: ${movie.rating}",
+                            style = MaterialTheme.typography.caption
+                        )
+                    }
+                }
+
+                Icon(
+                    imageVector = if (expanded) Icons.Filled.KeyboardArrowUp else Icons.Filled.KeyboardArrowDown,
+                    contentDescription = "down arrow",
+                    modifier = Modifier
+                        .size(25.dp)
+                        .clickable { expanded = !expanded },
+                    tint = Color.DarkGray
+                )
+            }
         }
     }
 }
